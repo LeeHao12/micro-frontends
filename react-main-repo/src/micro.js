5e5ce90a5532f852d3dfee4b4eb834e9ff91a4e3
leehao <lee_haoo@163.com>
2021-07-28 10:23:35 +0800

    feat: repo code

--- react-main-repo/src/micro.js
+++ react-main-repo/src/micro.js
@@ -2,58 +2,68 @@ import {
   registerMicroApps,
   addGlobalUncaughtErrorHandler,
   start,
+  initGlobalState,
 } from "qiankun";
 
-// 微应用注册信息
-const apps = [
-  /**
-   * name: 微应用名称 - 具有唯一性
-   * entry: 微应用入口 - 通过该地址加载微应用
-   * container: 微应用挂载节点 - 微应用加载完成后将挂载在该节点上
-   * activeRule: 微应用触发的路由规则 - 触发路由规则后将加载该微应用
-   */
-  {
-    name: "react-car-repo",
-    entry: "//localhost:10100",
-    container: "#frame",
-    activeRule: "/car",
-  },
-  {
-    name: "react-user-repo",
-    entry: "//localhost:10101",
-    container: "#frame",
-    activeRule: "/user",
-  },
-];
+// fetch api auth
+setTimeout(() => {
+  const apps = [
+    {
+      name: "react-car-repo",
+      entry: "//localhost:10100",
+      container: "#frame",
+      activeRule: "/car",
+      loader: (loading) => {
+        console.log("react-car-repo loading: ", loading);
+      },
+      props: {},
+    },
+    {
+      name: "react-user-repo",
+      entry: "//localhost:10101",
+      container: "#frame",
+      activeRule: "/user",
+      loader: (loading) => {
+        console.log("react-user-repo loading: ", loading);
+      },
+      props: {},
+    },
+  ];
 
-/**
- * 注册微应用
- * 第一个参数 - 微应用的注册信息
- * 第二个参数 - 全局生命周期钩子
- */
-registerMicroApps(apps, {
-  // qiankun 生命周期钩子 - 微应用加载前
-  beforeLoad: (app) => {
-    console.log("before load", app.name);
-    return Promise.resolve();
-  },
-  // qiankun 生命周期钩子 - 微应用挂载后
-  afterMount: (app) => {
-    // 加载微应用前，进度条加载完成
-    return Promise.resolve();
-  },
-});
+  registerMicroApps(apps, {
+    beforeLoad: (app) => {
+      console.log("before load: ", app.name);
+      return Promise.resolve();
+    },
+    afterMount: (app) => {
+      console.log("after mount: ", app.name);
+      return Promise.resolve();
+    },
+  });
+}, 1000);
 
-/**
- * 添加全局的未捕获异常处理器
- */
-addGlobalUncaughtErrorHandler((event) => {
+const errorHandler = (event) => {
   console.error(event);
   const { message: msg } = event;
   // 加载失败时提示
   if (msg && msg.includes("died in status LOADING_SOURCE_CODE")) {
     console.log("微应用加载失败，请检查应用是否可运行");
   }
+};
+
+// 加入 error 回调
+addGlobalUncaughtErrorHandler(errorHandler);
+
+// 状态，返回通信方法
+const StateActions = initGlobalState({
+  username: "lh11",
+  age: 18,
+  current: "main",
+});
+
+StateActions.onGlobalStateChange((state, prev) => {
+  console.log("react-main-repo state: ", state);
+  console.log("react-main-repo prevState: ", prev);
 });
 
 export default start;
